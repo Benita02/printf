246dad1324665858d140223bb602c47367b553cc
hbadejo <badejohakeem95@gmail.com>
2022-07-12 16:03:07 -0700

    Update tobase converter

--- test_files/convertToDecimal.c
+++ test_files/convertToDecimal.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * convertToDecimal - Convert base 8 and 16 to base 10
+ *
+ * Return: Decimal
+ */
 long long convertToDecimal(int Number, int base)
 {
     int decimalNumber = 0, i = 0;
